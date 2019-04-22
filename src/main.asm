d85b674293ca38b8373ac561e8f3189a454907cc
Tochigi <bailey.brownie60@gmail.com>
2019-04-21 21:58:20 -0400

    add shared.inc

--- src/main.asm
+++ src/main.asm
@@ -1,4 +1,6 @@
 [SECTION .data]
+%include "shared.inc"
+
 pauseString db "Press any key to continue . . . ", 0
 
 crlf db 0Dh, 0Ah, 0
@@ -22,43 +24,6 @@ extern _getch
 extern testfn
 
 global main
-
-%macro prologue 0
-	push ebp
-	mov ebp, esp
-%endmacro
-
-%macro epilogue 0
-	mov esp, ebp
-	pop ebp
-%endmacro
-
-%macro put 1-*
-	%rep %0
-		%rotate -1
-		push %1
-	%endrep
-	call printf
-	add esp, (%0*4)
-%endmacro
-
-%macro get 1
-	push %1
-	call gets
-	add esp, 4
-%endmacro
-
-%macro _pause 0
-	push pauseString
-	call printf
-	add esp, 4
-	
-	call _getch
-	
-	push crlf
-	call printf
-	add esp, 4
-%endmacro
 
 main:
 	prologue
--- src/test.asm
+++ src/test.asm
@@ -1,4 +1,6 @@
 [SECTION .data]
+%include "shared.inc"
+
 idkwhattocallthis db '>', 0
 
 fmtString db "You typed: %s", 0Dh, 0Ah, 0
@@ -10,31 +12,6 @@ teststring resb 256
 extern printf
 extern gets
 global testfn
-
-%macro prologue 0
-	push ebp
-	mov ebp, esp
-%endmacro
-
-%macro epilogue 0
-	mov esp, ebp
-	pop ebp
-%endmacro
-
-%macro put 1-*
-	%rep %0
-		%rotate -1
-		push %1
-	%endrep
-	call printf
-	add esp, (%0*4)
-%endmacro
-
-%macro get 1
-	push %1
-	call gets
-	add esp, 4
-%endmacro
 
 testfn:
 	prologue
